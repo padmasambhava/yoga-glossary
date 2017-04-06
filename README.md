a7113caf18b700d3fad696f98991276138ad748d
Pedro Morgan <pedromorgan@gmail.com>
2017-04-06 02:11:13 +0100

    lets go

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # yoga-glossary
-Yaml and alike formatted content
+
+## The cunning plan
+
+@pedromorgan the developer at Padma Olde South wales,
+and a meniw of terms and definitions, and 
+the (padma/tiki-index.php?page=Yoga%20Glossary)[http://www.padma.org.uk/tiki/tiki-index.php?page=Yoga%20Glossary
+is the root of this cunning plan.
+
+So this is ripe for consolidation and team community effort.
+looking at you atma@padma, KK@ richanrd et all...
+
--- fabfile.py
+++ fabfile.py
@@ -8,8 +8,9 @@ from fabric.contrib.console import confirm
 
 ROOT = os.path.dirname(os.path.abspath(__file__)) + "/"
 
+GLOSS_DIR = os.path.join(ROOT, "yoga_glossary")
 
-def stripped(parts):
+def _stripped(parts):
     ret = []
     for p in parts:
         s = p.replace("\t", "")
@@ -25,7 +26,7 @@ def stripped(parts):
 
 def imp():
     
-    a2z = {}
+    dic = {}
     
     
     with open("Raw.txt", "r") as f:
@@ -36,18 +37,53 @@ def imp():
         for raw_line in lines:
             line = raw_line.strip()
             if "–" in line:
+                # means we got a term - description
                 parts = line.split("–")
                 if len(parts) > 1:
-                    # strp
-                    sparts =  stripped(parts)				
-                    print sparts
-                    term = sparts[0]
-                    az = sparts[0][0].lower()
-                    if not az in a2z:
-                        a2z[az] = {}
+                    # split into parts
+                    sparts =  _stripped(parts)	
                     
-                    defs = sparts[1:]
-                    a2z[az] [term] = dict(defs=defs)
+                    # the terms is first elem
+                    term = sparts[0].replace("/", ", ")
+                    # the a is the first letter of term
+                    az = term[0].lower()
+                    a2z_dir = os.path.join(GLOSS_DIR, az)
+                    #print "=",  a2z_dir
+                    if not os.path.exists(a2z_dir):
+                        os.makedirs(a2z_dir)
+                    #data = a2z[a_char]
+                    
+                    
+                    # the defs are in elent t add
+                    defs = []
+                    defs_raw = " ".join(sparts[1:])
+                    
+                    ## rips ' of start and end
+                    if defs_raw.startswith("'") and defs_raw.endswith("'"):
+                        defs_raw = defs_raw[1:-1]
+                    if ";" in defs_raw:
+                        defs = _stripped(defs_raw.split(";"))
+                    else:
+                        defs = [defs_raw]
+                    
+                    stuff = dict(term=term, description=defs)
+                    term_file_name = term.replace(" ", "_").lower() + ".yaml"
+                    out_file = os.path.join(a2z_dir, term_file_name)
+                    
+                    ## we have to create bullshit comment
+                    ## and serialise the files aout in a particular order in our case
+                    # from this point forward, it is not expected
+                    # that the files should ba changed in any way
+                    # apart from version changes, and pull requests etc etc..
+                    out_str = "term: %s\n" % term
+                    out_str += "definition: \n"
+                    for da in defs:
+                        out_str += "  - %s\n" % da
+                        
+                    print out_file
+                    
+                    with open(out_file, "w") as ff:
+                        ff.write(out_str)
                     
                 else:
                     #print "  ??:", parts
@@ -55,20 +91,8 @@ def imp():
             else:
                 #print "   ?:", line
                 pass
-    print a2z.keys()		
-    ## Now we serialse to yaml
+    print dic.keys()		
 
-    for a_char in a2z:
-        dir_path = ROOT + a_char
-        print "=",  dir_path
-        if not os.path.exists(dir_path):
-            os.makedirs(dir_path)
-        data = a2z[a_char]
-        
-        yaml_str = yaml.dump(data)
-        file_name = a_char.replace(" ", "_") + ".yaml"
-        print file_name
-        with open(file_name, "w") as ff:
-            ff.write(yaml_str)
+
             
         
